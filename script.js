ac17568239a6542eed404414e4d28e39a4b87630
Shanica Denis <shanicanikad@gmail.com>
2021-01-23 13:02:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// /*I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character.*/
+
+// let iHaveADream = "I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character"
+
+// function wordArray(string) {
+//   return string.split(" ").reduce(function(count, word) {
+//     count[word] = count.hasOwnProperty(word) ? count[word] + 1 : 1;
+
+//     // '.split' splits the words in the string so that you just get each word separately.
+//     // '.reduce' give you each word/ output by itself.
+//     // '.hasOwProperty' give each word a boolean (whether value of each word is true or not).
+//     //  '+1' gives the actual value per word.
+
+//       return count;
+//     // 'return count' returns/ outputs the number value per word.
+
+//     }, {}); 
+// }
+// console.log(wordArray(iHaveADream))
+
+// // "console.log" shows the output for the string inside of the 'let' function. 
